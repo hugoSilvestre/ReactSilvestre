ca50a8227f9f9af064a487cf5742147363aa11b0
Hugo SILVESTRE <hugo.silvestre@outlook.fr>
2022-03-29 09:23:18 +0200

    Movies H1 Add

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -3,6 +3,7 @@ import React from "react";
 const Movies = (props) => {
     return(
         <div>
+            <h1>Mes Films</h1>
             <ul>
                 <h4>{props.elem.title}</h4>
                 <br></br>
